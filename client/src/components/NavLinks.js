04ab38a12349cb2191442945f36303829107211e
Sayan Mukherjee <hello@sayanmukherjee.com>
2022-04-26 17:02:26 +0530

    updated big and small sidebar

---
+++ client/src/components/NavLinks.js
@@ -0,0 +1,26 @@
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
+
+const NavLinks = ({ toggleSidebar }) => {
+  return (
+    <div className="nav-links">
+      {links.map((link) => {
+        const { text, url, id, icon } = link;
+        return (
+          <NavLink
+            to={url}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+            key={id}
+            onClick={toggleSidebar}
+          >
+            <span className="icon">{icon}</span>
+            {text}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
+};
+export default NavLinks;
--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,9 +1,28 @@
 import styled from "styled-components";
+import { FaTimes } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
+  const { toggleSidebar, showSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>SmallSidebar</h4>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
