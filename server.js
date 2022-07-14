9571be7aa45618734d6a9b3e6f2b0efbfd7805e8
Sayan Mukherjee <hello@sayanmukherjee.com>
2022-07-14 16:45:47 +0530

    updated changes and ready for production

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ import "express-async-errors";
 
 import morgan from "morgan";
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+
 // db and authenticate user
 import connectDB from "./db/connect.js";
 
@@ -23,14 +31,24 @@ import authenticatedUser from "./middleware/auth.js";
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello sayan");
-});
+app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticatedUser, jobsRouter);
+
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
