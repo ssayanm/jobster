3b576bc0be81bf07d95ef9862f32076279d4beb9
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-02-24 10:30:00 +0530

    updated the error and landing page

--- frontend/src/pages/HomeLayout.jsx
+++ frontend/src/pages/HomeLayout.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 export default function HomeLayout() {
   return (
     <div>
-      <h1>HomeLayout</h1>
       <Outlet />
     </div>
   );
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,4 +1,38 @@
 import styled from "styled-components";
+import Logo from "../components/Logo";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+
+export default function Landing() {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I&apos;m baby wayfarers hoodie next level taiyaki brooklyn cliche
+            blue bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+            narwhal.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+}
 
 const Wrapper = styled.section`
   nav {
@@ -47,7 +81,3 @@ const Wrapper = styled.section`
     }
   }
 `;
-
-export default function Landing() {
-  return <Wrapper>Landing</Wrapper>;
-}
