4b0d44dd57dd77d6cb2a9407bbb4ed5b604851f0
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-02-26 17:30:59 +0530

    working on the sidebar, added links and toggle for the small sidebar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import styled from "styled-components";
+import { FaAlignLeft } from "react-icons/fa";
+import Logo from "../components/Logo";
+import { useDashboardContext } from "../DashboardContext";
 
 export default function Navbar() {
-  return <Wrapper>Navbar</Wrapper>;
+  const { toggleSidebar } = useDashboardContext();
+
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <button type="button" className="toggle-btn" onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className="logo-text">dashboard</h4>
+        </div>
+        <div className="btn-container">toggle/logout</div>
+      </div>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.nav`
--- frontend/src/pages/DashboardLayout.jsx
+++ frontend/src/pages/DashboardLayout.jsx
@@ -11,7 +11,7 @@ export default function DashboardLayout() {
   const [isDarkTheme, setIsDarkTheme] = useState("false");
 
   const toggleDarkTheme = () => {
-    console.log("toggle dark theme");
+    setIsDarkTheme(!isDarkTheme);
   };
 
   const toggleSidebar = () => {
