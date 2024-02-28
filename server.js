9811e7cc773ed558f63707af77e0567e891b7ad6
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-02-28 07:42:29 +0530

    added, dotenv, morgan, nodemon

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 import express from "express";
+import morgan from "morgan";
+import * as dotenv from "dotenv";
+
 const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(5100, () => {
-  console.log("server is running...");
+app.post("/", (req, res) => {
+  console.log(req);
+
+  res.json({ message: "Data received", data: req.body });
+});
+
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`server running on PORT ${port}....`);
 });
