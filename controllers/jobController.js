2f05bc68216e96e9d7c792975364202b0dbf2e83
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-03-01 10:36:45 +0530

    updated job controller and custom errors

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -10,14 +10,12 @@ export const getAllJobs = async (req, res) => {
 export const createJob = async (req, res) => {
   // const { company, position } = req.body;
   const job = await Job.create(req.body);
-
-  // res.status(201).json({ job });
   res.status(StatusCodes.CREATED).json({ job });
 };
 
 export const getJob = async (req, res) => {
   const { id } = req.params;
-  const job = await Job.findById(id);
+  const job = await Job.findById(id.match(/^[0-9a-fA-F]{24}$/));
 
   if (!job) throw new NotFoundError(`no job with id : ${id}`);
   res.status(StatusCodes.OK).json({ job });
@@ -25,9 +23,13 @@ export const getJob = async (req, res) => {
 
 export const updateJob = async (req, res) => {
   const { id } = req.params;
-  const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
-    new: true,
-  });
+  const updatedJob = await Job.findByIdAndUpdate(
+    id.match(/^[0-9a-fA-F]{24}$/),
+    req.body,
+    {
+      new: true,
+    }
+  );
 
   if (!updatedJob) throw new NotFoundError(`no job with id ${id}`);
 
@@ -36,7 +38,7 @@ export const updateJob = async (req, res) => {
 
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
-  const removedJob = await Job.findByIdAndDelete(id);
+  const removedJob = await Job.findByIdAndDelete(id.match(/^[0-9a-fA-F]{24}$/));
   if (!removedJob) throw new NotFoundError(`no job with id ${id}`);
 
   res.status(StatusCodes.OK).json({ msg: "job deleted", job: removedJob });
