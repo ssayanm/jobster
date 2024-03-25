e8a15f34b421fd2aaa808d63f5299804f69d9984
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-03-25 09:57:50 +0530

    working on page container

--- frontend/src/components/PageBtnContainer.jsx
+++ frontend/src/components/PageBtnContainer.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
-import { useLocation, Link, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useAllJobsContext } from "../context/AllJobsContext";
 
 export default function PageBtnContainer() {
@@ -8,17 +8,55 @@ export default function PageBtnContainer() {
     data: { numOfPages, currentPage },
   } = useAllJobsContext();
 
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1;
+  });
+
   const { search, pathname } = useLocation();
   const navigate = useNavigate();
 
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
   return (
     <Wrapper>
       {" "}
-      <button className="btn prev-btn">
+      <button
+        className="btn prev-btn"
+        onClick={() => {
+          let prevPage = currentPage - 1;
+          if (prevPage < 1) prevPage = numOfPages;
+          handlePageChange(prevPage);
+        }}
+      >
         <HiChevronDoubleLeft />
         prev
       </button>
-      <button className="btn prev-btn">
+      <div className="btn-container">
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              className={`btn page-btn ${
+                pageNumber === currentPage && "active"
+              }`}
+              key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button
+        className="btn prev-btn"
+        onClick={() => {
+          let nextPage = currentPage + 1;
+          if (nextPage > numOfPages) nextPage = 1;
+          handlePageChange(nextPage);
+        }}
+      >
         <HiChevronDoubleRight />
         next
       </button>
