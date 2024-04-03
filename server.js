4dae5a05f05f5c4e9f3e80c6ddb13be8981d7170
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-04-03 09:14:22 +0530

    updated build files

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
-// app.use("/", (req, res) => {
-//   res.status(200).json({ msg: "Hello.. dont worry I am runing" });
-// });
+app.use("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
