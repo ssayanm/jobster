2234eb108f03e0bba63c43852a28edba724a9913
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-04-06 07:17:54 +0530

    updated logo component

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.svg";
 
 export default function Logo() {
-  return <img src={logo} alt="jobify" className="logo" />;
+  return (
+    <Link to="/dashboard">
+      <img src={logo} alt="jobify" className="logo" />;
+    </Link>
+  );
 }
