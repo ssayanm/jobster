5033f043ea392369fb594a3e190617763e197b20
Sayan Mukherjee <hello@sayanmukherjee.com>
2024-04-19 10:56:13 +0530

    updated server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ cloudinary.v2.config({
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 app.use(cookieParser());
 app.use(express.json());
